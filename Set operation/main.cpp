287287844f7b0228c36a5e302a1eee7d763ab35e
RehanGith <chrehan1007@gmail.com>
2024-03-19 20:48:15 +0500

    union and intersection operations

--- Set operation/main.cpp
+++ Set operation/main.cpp
@@ -3,14 +3,18 @@
 using namespace std;
 
 
-vector<int> merging(vector<int>& A, vector<int>& B) {
+vector<int> union_operation(vector<int>& A, vector<int>& B) {
 	vector<int> C;
-	int j = 0, k = 0, i = 0;
-	for (; i < A.size() + B.size(); i++) {
-		if (A.at(j) <= B.at(k))
+	int j = 0, k = 0;
+	while (true) {
+		if (A.at(j) < B.at(k))
 			C.push_back(A.at(j++));
-		else
+		else if (A.at(j) > B.at(k))
 			C.push_back(B.at(k++));
+		else {
+			C.push_back(A.at(j++));
+			k++;
+		}
 		if (j == A.size() || k == B.size())
 			break;
 	}
@@ -20,10 +24,27 @@ vector<int> merging(vector<int>& A, vector<int>& B) {
 		C.push_back(B.at(k));
 	return C;
 }
+
+vector<int> intersection_operation(vector<int>& A, vector<int>& B) {
+	vector<int> C;
+	int j = 0, k = 0, i = 0;
+	while(true) {
+		if (A.at(j) == B.at(k)) {
+			C.push_back(A.at(j++));
+			k++;
+		}else if (A.at(j) < B.at(k))
+			j++;
+		else if (A.at(j) > B.at(k))
+			k++;
+		if (j == A.size() || k == B.size())
+			break;
+	}
+	return C;
+}
 int main() {
-	vector<int> A{ 1,5,7,9 };
-	vector<int> B{ 2,4,8,10,20 };
-	vector<int> res = merging(A, B);
+	vector<int> A{2,3,4,5,9,10,12,29 };
+	vector<int> B{ 1,3,9,12 };
+	vector<int> res = intersection_operation(A, B);
 	for (int num : res) {
 		cout << num << " ";
 	}
